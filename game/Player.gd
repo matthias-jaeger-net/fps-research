6dfb68ff1544cb58979776280f558754ff701c6f
Kiberbit <kiberbit@gmail.com>
2022-08-25 19:39:06 +0200

    basic movement

--- game/Player.gd
+++ game/Player.gd
@@ -1,14 +1,63 @@
 extends KinematicBody
 
+const MOUSE_SENS_X := -0.001
+const MOUSE_SENS_Y := -0.001
+const MIN_CAM_ANGLE = -85
+const MAX_CAM_ANGLE = 85
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+const MAX_SPEED = 9
+const MAX_SPEED_SPRINT = 15
+const ACCELERATION = 6
+const DECCELERATION = 8
+const JUMP_SPEED = 6
 
+onready var head: Spatial = $Head
+
+onready var gravity = -ProjectSettings.get_setting("physics/3d/default_gravity")
+
+var velocity: Vector3 = Vector3(0, 0, 0)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+func _unhandled_input(event):
+	if event is InputEventMouseMotion:
+		rotate_y(MOUSE_SENS_X * event.relative.x)
+		head.rotate_x(MOUSE_SENS_Y * event.relative.y)
+		head.rotation.x = clamp(head.rotation.x, deg2rad(MIN_CAM_ANGLE), deg2rad(MAX_CAM_ANGLE))
+
+func _physics_process(delta: float):
+	process_movement(delta)
+
+func process_movement(delta: float):
+	var dir := Vector3()
+	dir.x = Input.get_action_strength("right") - Input.get_action_strength("left")
+	dir.z = Input.get_action_strength("back") - Input.get_action_strength("forward")
+	
+	dir = global_transform.basis.xform(dir)
+	
+	if dir.length_squared() > 1:
+		dir /= dir.length()
+	
+	velocity.y += delta * gravity
+	
+	var hvel := velocity
+	hvel.y = 0
+	
+	var target_speed = MAX_SPEED_SPRINT if Input.is_action_pressed("sprint") else MAX_SPEED
+	var target_velocity = dir * target_speed
+	var accel = ACCELERATION if target_velocity.dot(hvel) > 0 else DECCELERATION
+	
+	hvel = hvel.linear_interpolate(target_velocity, accel * delta)
+	
+	velocity.x = hvel.x
+	velocity.z = hvel.z
+	
+	velocity = move_and_slide(velocity, Vector3.UP)
+	
+	if is_on_floor() and Input.is_action_just_pressed("jump"):
+		velocity.y = JUMP_SPEED
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
