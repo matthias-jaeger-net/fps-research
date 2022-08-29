3e13fbb071fa7da31a93dfe0d8dd476975bd9f0c
Matthias Jäger <write@matthias-jaeger.net>
2022-08-29 18:30:11 +0200

    Added demo level and blender files

--- game/Player.gd
+++ game/Player.gd
@@ -1,15 +1,15 @@
 extends KinematicBody
 
-const MOUSE_SENS_X := -0.001
-const MOUSE_SENS_Y := -0.001
+const MOUSE_SENS_X := -0.01
+const MOUSE_SENS_Y := -0.01
 const MIN_CAM_ANGLE = -85
 const MAX_CAM_ANGLE = 85
 
-const MAX_SPEED = 9
-const MAX_SPEED_SPRINT = 15
+const MAX_SPEED = 12
+const MAX_SPEED_SPRINT = 26
 const ACCELERATION = 6
 const DECCELERATION = 8
-const JUMP_SPEED = 6
+const JUMP_SPEED = 20
 
 onready var head: Spatial = $Head
 
@@ -18,14 +18,19 @@ onready var gravity = -ProjectSettings.get_setting("physics/3d/default_gravity")
 var velocity: Vector3 = Vector3(0, 0, 0)
 
 # Called when the node enters the scene tree for the first time.
-func _ready():
-	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+#  func _ready():
+	# Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	# pass
 
 func _unhandled_input(event):
 	if event is InputEventMouseMotion:
 		rotate_y(MOUSE_SENS_X * event.relative.x)
 		head.rotate_x(MOUSE_SENS_Y * event.relative.y)
-		head.rotation.x = clamp(head.rotation.x, deg2rad(MIN_CAM_ANGLE), deg2rad(MAX_CAM_ANGLE))
+		head.rotation.x = clamp(
+			head.rotation.x, 
+			deg2rad(MIN_CAM_ANGLE), 
+			deg2rad(MAX_CAM_ANGLE)
+		)
 
 func _physics_process(delta: float):
 	process_movement(delta)
@@ -59,7 +64,3 @@ func process_movement(delta: float):
 	if is_on_floor() and Input.is_action_just_pressed("jump"):
 		velocity.y = JUMP_SPEED
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
